7834814f1cee9a066c71c98d126d17ec40886725
Zsolt Tóth <waysofdarkness@gmail.com>
2022-03-18 20:21:03 +0100

    Changed usage of constants to environmental variables.

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,12 @@
 import {readFileSync} from 'fs';
-const {
+/*const {
     token,
     guildId,
     clientId,
     RestHttp
-} = JSON.parse(readFileSync(process.env.AUTHLOC ? process.env.AUTHLOC : './auth.json').toString());
+} = JSON.parse(readFileSync(process.env.AUTHLOC ? process.env.AUTHLOC : './auth.json').toString());*/
+const token = process.env.token;
+const guildId = process.env.token;
+const clientId = process.env.token;
+const RestHttp = process.env.token;
 export { token, guildId, clientId, RestHttp }
