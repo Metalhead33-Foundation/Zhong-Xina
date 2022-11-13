8a3c731f678cc66f5ad1e7c3378390a93de59869
Metalhead33 <waysofdarkness@gmail.com>
2022-11-13 10:23:32 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ import {readFileSync} from 'fs';
     RestHttp
 } = JSON.parse(readFileSync(process.env.AUTHLOC ? process.env.AUTHLOC : './auth.json').toString());*/
 const token = process.env.token;
-const guildId = process.env.token;
-const clientId = process.env.token;
-const RestHttp = process.env.token;
+const guildId = process.env.guildId;
+const clientId = process.env.clientId;
+const RestHttp = process.env.RestHttp;
 export { token, guildId, clientId, RestHttp }
